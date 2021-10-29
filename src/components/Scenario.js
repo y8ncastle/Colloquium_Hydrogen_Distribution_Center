fc0e060434b105ec32727a15d065c6546128a547
regina-kim <hlbrie21@gmail.com>
2021-10-29 16:13:07 +0900

    build scene selecting function and make scene0: loading

--- src/components/Scenario.js
+++ src/components/Scenario.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import Scene0 from './scene/Scene0';
+
+let scene = 0;  //0 to 10
+function selectScene(value){
+    if (value === 0) return <Scene0></Scene0>
+    else            return <h6>undefined</h6>
+}
 
 class Scenario extends Component {
     render() {
         return (
                 <div class="col-lg-8">
-                    <div class="card shadow mb-4">
+                    <div class="card shadow mb-4"> 
                         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                             <h6 class="m-0 font-weight-bold text-primary">Scenario</h6>
                         </div>
                         <div class="card-body">
                             <div class="chart-area">
+                            { 
+                                selectScene(scene)
+                            }               
                             </div>
                         </div>
                     </div>
---
+++ src/components/scene/Scene0.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Scene1 extends Component {
+    render() {
+        return (
+            <div>
+                <img src="/assets/car.png" height="50" width = "50" alt="car"/>
+                <h3>Loading...</h3>
+            </div>
+        )
+    }
+}
+export default Scene1;
