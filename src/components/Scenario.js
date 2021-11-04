d3a24c1b60061956540f3eda2fface1cd699592a
regina-kim <hlbrie21@gmail.com>
2021-11-05 05:10:38 +0900

    add new scenes to the Scenario.js

--- src/components/Scenario.js
+++ src/components/Scenario.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import Scene0 from './scene/Scene0';
+import Scene1 from './scene/Scene1';
+import Scene2 from './scene/Scene2';
+import Scene3 from './scene/Scene3';
+import Scene4 from './scene/Scene4';
+import Scene5 from './scene/Scene5';
+import Scene6 from './scene/Scene6';
+import Scene7 from './scene/Scene7';
+import Scene8 from './scene/Scene8';
+import Scene9 from './scene/Scene9';
 
-let sceneNum = 0;  //0 to 9
+let sceneNum = 9;  //0 to 9
 function selectScene(value){
 
     if(!value && value!==0)  return <h3>undefined</h3>
     switch (value){
-        case 0:
-            return <Scene0></Scene0>
+        case 0: return <Scene0></Scene0>
+        case 1: return <Scene1></Scene1>
+        case 2: return <Scene2></Scene2>
+        case 3: return <Scene3></Scene3>
+        case 4: return <Scene4></Scene4>
+        case 5: return <Scene5></Scene5>
+        case 6: return <Scene6></Scene6>
+        case 7: return <Scene7></Scene7>
+        case 8: return <Scene8></Scene8>
+        case 9: return <Scene9></Scene9>
         default:
-            return <h3>default</h3>
+            return <h3>Unvalid Input</h3>
     }
     
 }
